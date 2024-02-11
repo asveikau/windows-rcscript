521eae7e29de9e247314c82f5d2d2a85c4908908
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2024-02-11 12:04:21 -0800

    vcvarsall.cmd: Add error checking

--- bin/vcvarsall.cmd
+++ bin/vcvarsall.cmd
@@ -7,4 +7,13 @@ cd /d "%ProgramFiles(x86)%"
 for /f "tokens=*" %%x in ('dir /s vcvarsall.bat /b') do set VCVARSALL=%%x
 popd
 
+if "%VCVARSALL%"=="" goto :fail
+
 "%VCVARSALL%" %*
+
+set VCVARSALL=
+goto :ok
+:fail
+echo Could not find vcvarsall.bat. 1>&2
+exit /b 1
+:ok
